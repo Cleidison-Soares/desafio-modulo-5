909b232895834971eb40e81365e103c24f37eb7b
cleidison-soares <cleidison33@gmail.com>
2023-04-18 01:23:37 -0300

    alteraçao na conexao com banco de dados

--- src/conexao/knex.js
+++ src/conexao/knex.js
@@ -1,12 +1,15 @@
+
+
 const knex = require("knex")({
-  client: process.env.KNEX_CLIENT,
+  client: 'pg',
   connection: {
-    host: process.env.KNEX_HOST,
-    port: process.env.KNEX_PORT,
-    user: process.env.KNEX_USER,
-    password: process.env.KNEX_PASSWORD,
-    database: process.env.KNEX_DATABASE,
+    host: 'containers-us-west-53.railway.app',
+    port: '6490',
+    user: 'postgres',
+    password: 'mf8de6B9LpHFERLwfwWd',
+    database: 'railway',
   },
-});
+
+})
 
 module.exports = knex;
